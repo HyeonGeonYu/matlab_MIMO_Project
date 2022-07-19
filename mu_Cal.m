8a3a0f1d2f85a47e672af070efa78096aa22f668
HyeonGeon <yuhyeongeon@gmail.com>
2022-07-19 19:38:31 +0900

    add Ex_3,4,5

--- mu_Cal.m
+++ mu_Cal.m
@@ -1,20 +1,32 @@
-function mu = mu_Cal(Lambda_matrix,W_matrix,p_0)
+function [mu,diag_f_matrix] = mu_Cal(Lambda_matrix,W_matrix,p_0,B)
     
     
-  for iter_ = 1: 5 % rank(H)
-    rho_vector = diag( Lambda_matrix ) .* diag( W_matrix );
+  for iter_ = 0: B-1 %
+    
+    Lambda_vector = diag( Lambda_matrix);
+    W_vector =diag( W_matrix );
+    rho_vector = Lambda_vector .* W_vector;
 
-    mu = ( (sum( diag( Lambda_matrix^(-0.5) )  .* diag( W_matrix^(0.5) ) ))...
-     / ( p_0 + sum( diag( Lambda_matrix^(-1) ) ) ) )^2;
+    mu = ( (sum( Lambda_vector(1:B-iter_).^(-0.5) .* W_vector(1:B-iter_).^(0.5)  ))...
+     / ( p_0 + sum( Lambda_vector(1:B-iter_).^(-1) ) ) )^2;
     
     
-    if all(rho_vector - mu>0)
+    if all(rho_vector(1:B-iter_)  - mu>0)
       break
     end
     
   end
 
-  if ~all(rho_vector - mu>0) 
+  diag_f_matrix = ( mu^(-0.5) * Lambda_matrix^(-0.5) * W_matrix^(0.5)...
+   - (Lambda_matrix)^(-1))^(0.5);
+  
+  diag_f_tmp = diag(diag_f_matrix);
+  diag_f_tmp(B+1-iter_:B) = 0 ;
+  diag_f_matrix = diag(diag_f_tmp);
+
+  diag_f_matrix(diag_f_matrix<0) = 0;
+
+  if ~all(rho_vector(1:B-iter_)  - mu>0)
      error("mu value does not exist")
   end
 end
