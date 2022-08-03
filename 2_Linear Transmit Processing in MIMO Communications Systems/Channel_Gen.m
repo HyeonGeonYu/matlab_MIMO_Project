8a2b22eabf0e98c74b8f08db7a3873ed1ea41726
HyeonGeon <yuhyeongeon@gmail.com>
2022-08-03 18:58:30 +0900

    2_commit

---
+++ 2_Linear Transmit Processing in MIMO Communications Systems/Channel_Gen.m
@@ -0,0 +1,13 @@
+function [H] = Channel_Gen(SP)
+
+    Nr = SP.Nr;
+    Nt = SP.Nt;
+    H_type = SP.H_type;
+
+    switch H_type
+        
+        case 'Rayleigh'
+            H = 1/sqrt(2)*(randn(Nr,Nt) + 1j*randn(Nr,Nt));
+    end
+    
+    end
